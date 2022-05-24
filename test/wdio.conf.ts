f1e3bc92140d876944e470d2a598fe06189f79e4
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-05-24 16:03:39 +0530

    Enabled Allure Report

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -159,7 +159,16 @@ export const config: Options.Testrunner = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: [
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: false,
+        disableWebdriverScreenshotsReporting: false,
+      },
+    ],
+  ],
 
   //
   // Options to be passed to Mocha.
