6d3f8e830f56a24c9578a966a827997df79659ed
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-05-24 15:50:25 +0530

    Added invalid scenario tests

--- test/pageobjects/LoginPage.ts
+++ test/pageobjects/LoginPage.ts
@@ -15,6 +15,10 @@ class LoginPage {
     return $("div[id=inventory_container]");
   }
 
+  protected get errorMessage() {
+    return $('h3[data-test="error"]');
+  }
+
   public async login(username, password) {
     await this.username.setValue(username);
     await this.password.setValue(password);
@@ -24,6 +28,14 @@ class LoginPage {
   public async isUserLoggedIn() {
     await expect(browser).toHaveUrl("https://www.saucedemo.com/inventory.html");
     await expect(this.inventoryContainer).toBePresent();
+  }
+
+  public async isErrorMessageAvailable() {
+    console.log(this.errorMessage.getText());
+    await expect(this.errorMessage).toBePresent();
+    await expect(this.errorMessage).toHaveTextContaining(
+      "Username and password do not match any user in this service"
+    );
   }
 }
 
--- test/specs/login.test.ts
+++ test/specs/login.test.ts
@@ -6,4 +6,10 @@ describe("Login Test", function () {
     await App.LoginPage.login("standard_user", "secret_sauce");
     await App.LoginPage.isUserLoggedIn();
   });
+
+  it("should not be able to login with invalid credentials", async function () {
+    await App.HomePage.visit();
+    await App.LoginPage.login("standard_user", "secret_sauce1");
+    await App.LoginPage.isErrorMessageAvailable();
+  });
 });
